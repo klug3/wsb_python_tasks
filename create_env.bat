882a86e194b7a270a35fe7129f4685ee0a65b10e
Uncle <tomasz.madry@amu.edu.pl>
2023-07-02 06:45:38 +0200

    notebook and prettytable added

--- create_env.bat
+++ create_env.bat
@@ -2,6 +2,7 @@ pause
 call py -m venv .venv
 call .venv/Scripts/activate
 call python -m pip install pip-tools
-call python -m pip install jupyter notebook
+call python -m pip install notebook
+call python -m pip install prettytable
 call python -m ipykernel install --user --name=.venv
 pause
